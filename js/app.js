619f51b432b476ee60fcd06e875ae762ab512810
HP <yacoubaboro1@gmail.com>
2023-05-15 18:17:00 +0000

    second commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// // alert("Shareholder");
+// let Foner = 35;
+// let pret = 14;
+// let BORO= Foner + pret;
+// alert(BORO);
+// let azerty =5;
+// if(azerty>1){
+//     console.log('La variable azerty est strictement plus grand que 1.');
+// }
+// let age =prompt('entrer votre age');
+// if(age>18){
+//     console.log('majeur');
+// }else   if(age<18){
+//     console.log('mineur');
+// }else{
+//     console.log('responsable');
+// }
+// let age =15;
+// for(let age =0; age<30; age++) {
+//     console.log(age);
+// }
+const apprentissage = document.getElementById("test");
+const couleur = apprentissage.style.color='blue';
+apprentissage.innerText='Le code vient de changer'
+console.log(apprentissage)
